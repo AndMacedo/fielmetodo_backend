43831ad5b979b2b23512ab36a4fc3ef383fd8f74
André Macedo <asilvamacedo@gmail.com>
2023-10-18 19:15:18 +0100

    async

--- src/controllers/ContactController.js
+++ src/controllers/ContactController.js
@@ -1,9 +1,9 @@
 const ContactModel = require("../models/ContactModel.js");
 
-const EmitContact = async(req, res) => {
+const EmitContact = (req, res) => {
   const { Email, FirstName, LastName, Phone, Notes } = req.body;
   if (req.body !== undefined) {      
-      await ContactModel.createEmail(Email, FirstName, LastName, Phone, Notes)
+      ContactModel.createEmail(Email, FirstName, LastName, Phone, Notes)
       res.sendStatus(200)    
     } else {
       console.error(res);
